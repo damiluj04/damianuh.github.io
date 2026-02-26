ace5232bb27bed9d93e38bc886349a07ab8f0192
damiluj04 <167890462+damiluj04@users.noreply.github.com>
2026-02-25 19:14:35 -0600

    Update hw1.js

--- hw1.js
+++ hw1.js
@@ -3,7 +3,7 @@ let text = d.toLocaleDateString();
 document.getElementById("today").innerHTML = text;
 
 let slider = document-getElementById(" range")
-    let output = document-getElementById(" range-scale")
+    let output = document-getElementById(" range-slider")
     output.innerTML = slider.value;
 
     slider. oninput = function () foutput. innerTML = this. value;};

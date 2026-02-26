b32e71d9964e49fc3a8113ba1cd672a68147cf6e
damiluj04 <167890462+damiluj04@users.noreply.github.com>
2026-02-25 19:12:08 -0600

    Update hw1.js
    Fix errors in range slider element retrieval and output display.

--- hw1.js
+++ hw1.js
@@ -1,3 +1,9 @@
 const d = new Date();
 let text = d.toLocaleDateString();
 document.getElementById("today").innerHTML = text;
+
+let slider = document-getElementById(" range")
+    let output = document-getElementById(" range-scale")
+    output.innerTML = slider.value;
+
+    slider. oninput = function () foutput. innerTML = this. value;};
